1e427add573beaf6b1a35d5c07b6b3067b348954
Tuukka Uimarihuhta <tuimarih@edu.lapinamk.fi>
2023-05-27 08:15:20 +0300

    variables in scopes, variable declarations as list, reference use

--- CPP-Learning/CPP-Learning.cpp
+++ CPP-Learning/CPP-Learning.cpp
@@ -5,4 +5,21 @@ int main()
     std::cout << "2\115\n";
 
     std::cout << "2\t\115\n";
+
+    int i = 100, sum = 0;
+    for (int i = 0; i != 10; ++i)
+        sum += i;
+    /* Sum will be 45, but because std::cout is outside for loops
+    scope and i is declared 100, i will be written as 100 instead of 10 */
+    std::cout << i << " " << sum << std::endl;
+
+    // declare multiple types of same variables in a list
+    double lice{ 12.12 }, vice{ 15.15 }, dice{ 20.20 };
+    std::cout << lice + vice + dice << std::endl;
+
+    double dval{ 3.14 };
+    // reference marked with & at start of name, reference has to initialize with object
+    double& refVal{ dval };
+    std::cout << refVal << std::endl;
+
 }
