dff547f0e1ba2bf46057ad508fe1fe131b6f1890
Tuukka Uimarihuhta <tuimarih@edu.lapinamk.fi>
2023-05-28 15:23:39 +0300

    Exercise 2.29

---
+++ CPP-Learning/2.28.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int e2_28() {
+
+	{
+		//(a)
+		//Gives an error, const pointer has to be initialized
+		//int i, *const cp;
+	}
+	{
+		//(b)
+		//Error, const pointer needs to be initialized
+		//int *p1, *const p2;
+	}
+	{
+		//(c)
+		//Error, ic needs to be initialized due to being const
+		const int ic, &r{ ic };
+	}
+	{
+		//(d)
+		//Error, const pointer requires initializer
+		//const int *const p3;
+	}
+	{
+		//(e)
+		//OK, p may point to a int that is const
+		const int *p;
+	}
+}
---
+++ CPP-Learning/2.29.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int e2_29() {
+		int i, *const cp{ &i };
+		int *p1, *const p2{ p1 };
+		const int ic{ 26 }, &r{ ic };
+		const int *const p3{ &ic };
+		const int *p;
+
+		{
+			//(a)
+			//OK, int can be changed to have the value of a const variable
+			i = ic;
+		}
+		{
+			//(b)
+			//Error, const int* cannot be assigned to type int
+			//p1 = p3;
+		}
+		{
+			//(c)
+			//Error, const int* cannot be assigned to type int
+			//p1 = &ic;
+		}
+		{
+			//(d)
+			//Error, const variable cannot be modified
+			//p3 = &ic;
+		}
+		{
+			//(e)
+			//Error, const variable cannot be modified
+			//p2 = p1;
+		}
+		{
+			//(f)
+			//Error, const variable cannot be modified
+			//ic = *p3;
+		}
+}
